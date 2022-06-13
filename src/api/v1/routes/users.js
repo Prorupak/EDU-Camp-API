5087267dc31f77128fd36cd19f22140d68a3f852
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-06-13 12:45:12 +0545

    users, middlewares error fixed

---
+++ src/api/v1/routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
+
+const User = require("../models/User");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middlewares/advancedResults");
+const { protect, authorize } = require("../middlewares/auth");
+
+router.use(protect);
+router.use(authorize("admin"));
+
+router.route("/").get(advancedResults(User), getUsers).post(createUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
